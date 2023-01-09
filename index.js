b4f4d5068feef5bed24e0ae2c4db39fe8d7a3f05
Tristan Teo <teo.jia.jun.29@gmail.com>
2023-01-09 23:00:11 +0800

    recreate fcl error (mainnet settings)

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ import { send as grpcSend } from '@onflow/transport-grpc';
 
 fcl
   .config()
-  .put('accessNode.api', 'https://access-testnet.onflow.org')
+  .put('accessNode.api', 'https://access.onflow.org/')
   .put('app.detail.title', 'Puddle')
   .put('decoder.Type', (val) => val.staticType)
   .put('decoder.Enum', (val) => Number(val.fields[0].value.value))
   .put('sdk.transport', grpcSend)
-  .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn')
-  .put('flow.network', 'testnet');
+  .put('discovery.wallet', 'https://fcl-discovery.onflow.org/authn')
+  .put('flow.network', 'mainnet');
 
 
 console.log("getting account now")
-const { account } = await fcl.send([fcl.getAccount('0xe6d966a5544e2d91')]);
+const { account } = await fcl.send([fcl.getAccount('0x0bf4e5204a578813')]);
 console.log("account", account)
